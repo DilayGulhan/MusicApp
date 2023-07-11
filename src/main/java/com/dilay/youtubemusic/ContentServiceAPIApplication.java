331bfd01dd76ef33d985cbd710fd17448961916a
DilayGulhan <dilaygulhan@gmail.com>
2023-07-11 22:01:43 +0300

    main

--- src/main/java/com/dilay/youtubemusic/ContentServiceAPIApplication.java
+++ src/main/java/com/dilay/youtubemusic/ContentServiceAPIApplication.java
@@ -2,10 +2,8 @@ package com.dilay.youtubemusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-
 public class ContentServiceAPIApplication {
 
     public static void main(String[] args) {
